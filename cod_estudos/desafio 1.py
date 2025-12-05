9daad020ac385292c8dc83becc47f9e7f195f1b6
arthurmourade <arthurmouradea@gmail.com>
2025-12-05 12:02:30 -0300

    Desafios de estudo em python

---
+++ cod_estudos/desafio 1.py
@@ -0,0 +1,6 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+cidade = input("Em qual cidade você mora? ")
+
+print("Olá, " + nome + "!")
+print("Você tem " + str(idade) + " anos e mora em " + cidade + ".")
